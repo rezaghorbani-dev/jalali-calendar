73feb10f4cd4c475872ce4ffb45f2ee1f9eb4317
Reza Ghorbani <rg@rezaghorbani.dev>
2024-07-01 13:49:35 +0330

    prepare for npm packaging

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+declare class App extends React.Component {}
+
+export default { App };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ rollupTypes: true })],
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      name: "react-beautiful-timeline",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "tailwindcss"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+
+
+  server: {
+    port: 3001,
+    open: "/demo/index.html"
+  }
+});
